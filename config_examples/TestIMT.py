4c27ca65e76e1d6c5d615022ffc4af21473daade
Immortality-IMT <info@imt.cx>
2025-07-15 13:19:51 +0700

    Improved test strategy

--- config_examples/TestIMT.py
+++ config_examples/TestIMT.py
@@ -16,16 +16,34 @@ class TestIMT(IStrategy):
     exit_tag = "exit"
 
     def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        dataframe["enter_tag"] = ""
+        dataframe["exit_tag"] = ""
+        dataframe["enter_tag"] = dataframe["enter_tag"].astype(object)
+        dataframe["exit_tag"] = dataframe["exit_tag"].astype(object)
         return dataframe
 
     def populate_entry_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-        # Use numpy for random generation
-        dataframe["enter_long"] = np.random.rand(len(dataframe)) < 0.5
-        dataframe["enter"] = np.random.rand(len(dataframe)) < 0.5
+        dataframe["enter_long"] = 0
+        dataframe.loc[dataframe.index[-1], "enter_long"] = 1
+        dataframe["enter_tag"] = ""
+        dataframe.loc[dataframe.index[-1], "enter_tag"] = "force-entry"
+
+        """
+        # Randomize only for last row
+        dataframe["exit_long"] = 0
+        if np.random.rand() < 0.5:
+            dataframe.iloc[-1, dataframe.columns.get_loc("enter_long")] = 1
+            dataframe.iloc[-1, dataframe.columns.get_loc("enter_tag")]  = "random-exit"
+        else:
+            dataframe.iloc[-1, dataframe.columns.get_loc("enter_tag")]  = ""
+        """
         return dataframe
 
     def populate_exit_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-        # Use numpy for random generation
-        dataframe["exit_long"] = np.random.rand(len(dataframe)) < 0.5
-        dataframe["exit"] = np.random.rand(len(dataframe)) < 0.5
+        dataframe["exit_long"] = 0
+        if np.random.rand() < 0.5:
+            dataframe.iloc[-1, dataframe.columns.get_loc("exit_long")] = 1
+            dataframe.iloc[-1, dataframe.columns.get_loc("exit_tag")] = "random-exit"
+        else:
+            dataframe.iloc[-1, dataframe.columns.get_loc("exit_tag")] = ""
         return dataframe
